874eb1c6a1adf819c99ba2f11f417830011e1755
Tarjányi József <tarjanyi.jozsef@freemail.hu>
2019-08-15 23:33:48 +0200

    #2 működik de tele van hibákkal

--- app/src/main/java/com/tarjanyi/vonalkodolvaso/MainActivity.kt
+++ app/src/main/java/com/tarjanyi/vonalkodolvaso/MainActivity.kt
@@ -5,27 +5,33 @@ import android.content.Context
 import android.content.pm.PackageManager
 import android.os.Bundle
 import android.os.Vibrator
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import com.google.zxing.Result
+import com.tarjanyi.vonalkodolvaso.R.layout.activity_main
 import me.dm7.barcodescanner.zxing.ZXingScannerView
 
 class MainActivity : AppCompatActivity(), ZXingScannerView.ResultHandler {
     private val REQUEST_CAMERA = 1
     private var scannerView :ZXingScannerView?=null
+    private var txtResult : TextView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        scannerView= ZXingScannerView(this)
-        setContentView(scannerView)
+        setContentView(activity_main)
 
+        scannerView= findViewById(R.id.scanner)
+
+
+        txtResult = findViewById(R.id.textResult)
         if (!checkPermission())
             requestPermission()
     }
 
     private fun checkPermission() : Boolean{
-        return ContextCompat.checkSelfPermission(  this@MainActivity,android.Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED
+        return ContextCompat.checkSelfPermission(  this@MainActivity, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED
     }
     private fun requestPermission(){
         ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.CAMERA),REQUEST_CAMERA)
@@ -35,6 +41,7 @@ class MainActivity : AppCompatActivity(), ZXingScannerView.ResultHandler {
         super.onResume()
         if (checkPermission()){
             if (scannerView == null){
+               scannerView= findViewById(R.id.scanner)
                scannerView = ZXingScannerView(this)
                 setContentView(scannerView)
             }
@@ -49,9 +56,13 @@ class MainActivity : AppCompatActivity(), ZXingScannerView.ResultHandler {
     }
     override fun handleResult(p0: Result?) {
         val result:String? = p0?.text
-        val vibrator:Vibrator = applicationContext.getSystemService(Context.VIBRATOR_SERVICE) as Vibrator
+        val vibrator = applicationContext.getSystemService(Context.VIBRATOR_SERVICE) as Vibrator
+        /*vibrator.vibrate( 100)*/
+        txtResult?.text = result
+        scannerView?.setResultHandler(this)
+        scannerView?.startCamera()
 
-        val builder = android.app.AlertDialog.Builder(this)
+        /*val builder = android.app.AlertDialog.Builder(this)
         builder.setTitle("Result")
         builder.setPositiveButton("OK") {dialog,which ->
             scannerView?.resumeCameraPreview (this@MainActivity)
@@ -59,7 +70,7 @@ class MainActivity : AppCompatActivity(), ZXingScannerView.ResultHandler {
         }
         builder.setMessage(result)
         val alertDialog = builder.create()
-        alertDialog.show()
+        alertDialog.show()*/
 
     }
 }
